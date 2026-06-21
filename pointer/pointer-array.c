0271c928628bd8cd4c4b0ffee28d5aadd4ee725c
praneshchowdhury <pranesh.ac.bd@gmail.com>
2026-06-21 21:39:10 +0600

    Add pointer-array.c in pointer folder

---
+++ pointer/pointer-array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int arr[3]={10,20,30};
+ 
+    int i;
+    for(i=0;i<3;i++){
+        printf("Index[%d] : value ==> %d (Address : %u)\n",i,*(arr+i),(arr+i));
+    }
+}
